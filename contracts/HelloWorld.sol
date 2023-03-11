9e5bf7149b0c04d02e37205ae5e6e3f66917bafb
Cássio Gabriel <106029918+CassivsGabriellis@users.noreply.github.com>
2023-03-11 10:12:36 -0300

    adjusting the comments

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -4,14 +4,18 @@
 pragma solidity >=0.7.3;
 
 // Defines a contract named `HelloWorld`.
-// A contract is a collection of functions and data (its state). Once deployed, a contract resides at a specific address on the Ethereum blockchain. Learn more: https://solidity.readthedocs.io/en/v0.5.10/structure-of-a-contract.html
+// A contract is a collection of functions and data (its state). Once deployed, a contract resides at a specific address 
+//on the Ethereum blockchain. Learn more: https://solidity.readthedocs.io/en/v0.5.10/structure-of-a-contract.html
 contract HelloWorld {
     //Emitted when update function is called
-    //Smart contract events are a way for your contract to communicate that something happened on the blockchain to your app front-end, which can be 'listening' for certain events and take action when they happen.
+    //Smart contract events are a way for your contract to communicate that something happened on the blockchain to your app front-end,
+    //which can be 'listening' for certain events and take action when they happen.
     event UpdatedMessages(string oldStr, string newStr);
 
     // Declares a state variable `message` of type `string`.
-    // State variables are variables whose values are permanently stored in contract storage. The keyword `public` makes variables accessible from outside a contract and creates a function that other contracts or clients can call to access the value.
+    // State variables are variables whose values are permanently stored in contract storage.
+    //The keyword `public` makes variables accessible from outside a contract and creates a function
+    //that other contracts or clients can call to access the value.
     string public message;
 
     // Similar to many class-based object-oriented languages, a constructor is a special function that is only executed upon contract creation.
