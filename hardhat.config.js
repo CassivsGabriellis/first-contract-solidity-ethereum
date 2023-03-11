eb33cfcc258ae76914388039f6e03a9edcfaf089
Cassivs Gabriellis <cassiogabrielcontato@gmail.com>
2023-03-11 20:51:55 -0300

    minor modifications

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,8 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 
 require('dotenv').config();
-require('@nomiclabs/hardhat-ethers');
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
 
 const { API_URL, PRIVATE_KEY } = process.env;
 
@@ -14,5 +15,9 @@ module.exports = {
       url: API_URL,
       accounts: [`0x${PRIVATE_KEY}`],
     },
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    apiKey: "5QWWDA6FCFFNA389RM84NM52SPUCRHDSXG", //
   },
 };
--- nota.md
+++ nota.md
@@ -1,3 +1,5 @@
-Endereço do primeiro contrato enviado: 0x8842A116e01093fC4692B90A9Ec661ce0BB06146
+Endereço do primeiro contrato enviado: 0xbFFBD52612077D1188d76d7773305CBd868518e9
+_Contract deployed to address: 0xbFFBD52612077D1188d76d7773305CBd868518e9_
 
 Site para visualizar o contrato: [Goerli Etherscan](https://goerli.etherscan.io/).
+_Access this site to visualize the contract: [Goerli Etherscan](https://goerli.etherscan.io/)._
